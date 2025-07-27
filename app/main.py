b4de8eb7ae6e6e6513a9f383dcfaed7c115df859
Alona Samotoi <meit109nerium@gmail.com>
2025-07-27 21:25:55 +0300

    Solution, corrected flake8

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     cash_dict = {}
 
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs) -> Any:
         key = (args, tuple(sorted(kwargs.items())))
         if key in cash_dict:
             print("Getting from cache")
@@ -17,5 +17,3 @@ def cache(func: Callable) -> Callable:
 
     return wrapper
     pass
-
-
